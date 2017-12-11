b85e8ce44410f703b16cafe35d2a67db3bcc093e
owen151128 <owen151128@gmail.com>
2017-12-11 11:25:59 +0900

    simpler_code

--- app/src/main/java/com/hpcnt/rxjavapractice/MainActivity.java
+++ app/src/main/java/com/hpcnt/rxjavapractice/MainActivity.java
@@ -5,9 +5,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
 import io.reactivex.Observable;
-import io.reactivex.annotations.NonNull;
-import io.reactivex.functions.Action;
-import io.reactivex.functions.Consumer;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,29 +13,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Observable<String> mObservable = Observable.just("Hello Rx!");
-
-        Consumer<String> onNextAction = new Consumer<String>() {
-            @Override
-            public void accept(@NonNull String s) throws Exception {
-                Log.e("onNextAction", s);
-            }
-        };
-
-        Action onCompleteAction = new Action() {
-            @Override
-            public void run() throws Exception {
-                Log.e("onCompleteAction", "onComplete!!");
-            }
-        };
-
-        Consumer<Throwable> onErrorAction = new Consumer<Throwable>() {
-            @Override
-            public void accept(@NonNull Throwable throwable) throws Exception {
-                Log.e("onErrorAction", throwable.getMessage());
-            }
-        };
-
-        mObservable.subscribe(onNextAction, onErrorAction, onCompleteAction);
+        Observable.just("Hello Rx!")
+                .subscribe(s -> Log.e("onNext", s));
     }
 }
