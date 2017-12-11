9eca37618219506d4d7caf2484217581c865e0fd
owen151128 <owen151128@gmail.com>
2017-12-11 12:40:16 +0900

    introducing_operators

--- app/src/main/java/com/hpcnt/rxjavapractice/MainActivity.java
+++ app/src/main/java/com/hpcnt/rxjavapractice/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Observable.just("Hello Rx!")
-                .subscribe(s -> Log.e("onNext", s + " " + "powered by RxJava..."));
+                .map(s -> s + " powerd by RxJava...")
+                .subscribe(s -> Log.e("onNext", s));
     }
 }
