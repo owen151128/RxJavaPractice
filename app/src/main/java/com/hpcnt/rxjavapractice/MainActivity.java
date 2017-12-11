76c4612b14340e3d9d1c4beba39941fbfc2cfac3
owen151128 <owen151128@gmail.com>
2017-12-11 11:05:24 +0900

    hello,world

--- app/src/main/java/com/hpcnt/rxjavapractice/MainActivity.java
+++ app/src/main/java/com/hpcnt/rxjavapractice/MainActivity.java
@@ -1,7 +1,14 @@
 package com.hpcnt.rxjavapractice;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.annotations.NonNull;
+import io.reactivex.functions.Consumer;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Observable<String> mObservable = Observable.create(new ObservableOnSubscribe<String>() {
+            @Override
+            public void subscribe(@NonNull ObservableEmitter<String> e) throws Exception {
+                e.onNext("Hello Rx!");
+                e.onComplete();
+            }
+        });
+
+        Consumer<String> mConsumer = new Consumer<String>() {
+            @Override
+            public void accept(@NonNull String s) throws Exception {
+                Log.e("accept", s);
+            }
+        };
+
+        mObservable.subscribe(mConsumer);
     }
 }
